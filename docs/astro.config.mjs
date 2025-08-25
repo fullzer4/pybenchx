a68f8f1d87f252c7f100d0da8ee1f2d5869ce7d0
fullzer4 <gabrielpelizzaro@gmail.com>
2025-08-24 23:00:25 -0300

    fix: deploy docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  site: 'https://fullzer4.github.io/pybenchx',
+  site: 'https://fullzer4.github.io/pybenchx/',
   base: '/pybenchx',
   integrations: [
     starlight({
